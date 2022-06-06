091cb4c9651b6464e597e1bb1ecdfa7cb17ae272
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-06-06 20:17:18 +0700

    Commit 2
    Begin make class Phone

--- phonebook_class/phone.h
+++ phonebook_class/phone.h
@@ -1,7 +1,22 @@
 #pragma once
-
-class phone
+#include<iostream>
+using namespace std;
+class Phone
 {
+private:
+	char * name;
+	char home_number[6];
+	char work_number[6];
+	char mobile_number[12];
+	char * contact_data;
+public:
+	Phone(char* nameP, char homeP[6], char workP[6], char mobileP[12], char* data) {
+		name = new char[strlen(nameP)+1];
+		for (int i = 0; i < strlen(nameP); i++) {
+			name[i] = nameP[i];
+		}
+		name[strlen(nameP) + 1] = '\0';
 
+	};
 };
 
